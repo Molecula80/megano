dfa0712e58f27281fad9313927c47d1db1796996
Asus <molecula80@gmail.com>
2022-09-15 22:34:31 +0300

    add styles

--- online_store/app_catalog/views.py
+++ online_store/app_catalog/views.py
@@ -2,10 +2,13 @@
 from django.views.generic import TemplateView, ListView, DetailView
 
 
-class MainPageView(TemplateView):
+class IndexView(TemplateView):
     """ Главная страница. """
+    template_name = 'app_catalog/index.html'
+
     def get_context_data(self, **kwargs):
-        pass
+        context = super().get_context_data(**kwargs)
+        return context
 
 
 class ProductListView(ListView):
