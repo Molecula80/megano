2357f9afd88029799d5fd70ea1ecda47787e8bee
Asus <molecula80@gmail.com>
2022-09-18 22:43:46 +0300

    add category model

--- online_store/app_catalog/admin.py
+++ online_store/app_catalog/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+    extra = 0
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'parent', 'sort_index', 'active']
+    list_filter = ['parent', 'sort_index', 'active']
+    search_fields = ['title']
+    inlines = [CategoryInline]
+    actions = ['mark_as_active', 'mark_as_inactive']
+
+    def mark_as_active(self, request, queryset):
+        queryset.update(active=True)
+
+    def mark_as_inactive(self, request, queryset):
+        queryset.update(active=False)
+
+    mark_as_active.short_description = 'Пометить как активные'
+    mark_as_inactive.short_description = 'Пометить как неактивные'
+
+
+admin.site.register(Category, CategoryAdmin)
--- online_store/app_catalog/models.py
+++ online_store/app_catalog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey('Category', blank=True, null=True, on_delete=models.SET_NULL, related_name='children',
+                               verbose_name='родитель')
+    title = models.CharField(max_length=255, verbose_name='название')
+    sort_index = models.PositiveIntegerField(verbose_name='индекс сортировки')
+    icon = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name='иконка')
+    active = models.BooleanField(default=False, verbose_name='активно')
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.title
