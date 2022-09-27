b01feb25a9c650e05c578847daad0914cded984f
Asus <molecula80@gmail.com>
2022-09-27 22:09:26 +0300

    add form error messages

--- online_store/app_users/forms.py
+++ online_store/app_users/forms.py
@@ -6,15 +6,16 @@ from .models import User
 
 class RegisterForm(UserCreationForm):
     """ Форма регистрации пользователя. """
-    full_name = forms.CharField(max_length=255, required=True, error_messages={'required': 'Заполните это поле!'},
+    full_name = forms.CharField(max_length=255, required=True,
+                                error_messages={'required': 'Это поле обязательно для заполнения!'},
                                 widget=forms.TextInput(attrs={"class": "form-input", "id": "name", "name": "name",
                                                               "type": "text", "value": "", "data-validate": "require"}))
     telephone = forms.CharField(max_length=255, required=False,
-                                error_messages={'unique': 'Пользователь с таким телефоном уже есть!'},
+                                error_messages={'unique': 'Пользователь с таким номером телефона уже есть!'},
                                 widget=forms.TextInput(attrs={"class": "form-input", "id": "phone", "name": "phone",
                                                               "type": "text", "value": ""}))
-    email = forms.EmailField(required=True, error_messages={'required': 'Заполните это поле!',
-                                                            'invalid': 'Недопустимое значение!',
+    email = forms.EmailField(required=True, error_messages={'required': 'Это поле обязательно для заполнения!',
+                                                            'invalid': 'Это значение недопустимо!',
                                                             'unique': 'Пользователь с таким email уже есть!'},
                              widget=forms.EmailInput(attrs={"class": "form-input", "id": "mail", "name": "mail",
                                                             "type": "text", "value": "", "data-validate": "require"}))
