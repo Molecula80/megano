192db83740667eeefb87e21e1c4d548bfac5fa35
Asus <molecula80@gmail.com>
2022-10-01 22:14:19 +0300

    add password reset form

--- online_store/app_users/urls.py
+++ online_store/app_users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView
+from django.contrib.auth.views import PasswordResetConfirmView, PasswordResetCompleteView
 
 from .views import register_view, UserLogoutView, login_view
 
@@ -8,4 +10,9 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    # Обработчики восстановления пароля.
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
