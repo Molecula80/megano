89062fc21925b71abbbe5555e55e8127f4fe107b
Asus <molecula80@gmail.com>
2022-10-18 22:52:30 +0300

    add new models to admin site

--- online_store/app_catalog/admin.py
+++ online_store/app_catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Fabricator, Product, DescrPoint, AddInfoPoint
 
 
 class CategoryInline(admin.TabularInline):
@@ -24,4 +24,91 @@ class CategoryAdmin(admin.ModelAdmin):
     mark_as_inactive.short_description = 'Пометить как неактивные'
 
 
+class ProductInline(admin.TabularInline):
+    model = Product
+    extra = 0
+
+
+class FabricatorAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    search_fields = ['title']
+    inlines = [ProductInline]
+
+
+class DescrPointInline(admin.TabularInline):
+    model = DescrPoint
+    extra = 0
+
+
+class AddInfoPointInline(admin.TabularInline):
+    model = AddInfoPoint
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'fabricator', 'price', 'num_purchases', 'sort_index', 'in_stock', 'free_delivery',
+                    'limited_edition']
+    list_filter = ['fabricator', 'sort_index', 'in_stock', 'free_delivery', 'limited_edition']
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [DescrPointInline, AddInfoPointInline]
+    actions = ['mark_as_in_stock', 'mark_as_out_of_stock', 'mark_as_free_delivery', 'mark_as_no_free_delivery',
+               'mark_as_limited_edition', 'mark_as_unlimited_edition']
+    fieldsets = (
+        ('Основные сведения', {
+            'fields': ('title', 'slug', 'description', 'price', 'image')
+        }),
+        ('Производитель и категории', {
+            'fields': ('fabricator', 'categories'),
+            'description': 'Сведения о производителе и категориях',
+            'classes': ['collapse']
+        }),
+        ('Дополнительные сведения', {
+            'fields': ('num_purchases', 'sort_index', 'added_at', 'in_stock', 'free_delivery', 'limited_edition'),
+            'classes': ['collapse']
+        })
+    )
+
+    def mark_as_in_stock(self, request, queryset):
+        queryset.update(in_stock=True)
+
+    def mark_as_out_of_stock(self, request, queryset):
+        queryset.update(in_stock=False)
+
+    def mark_as_free_delivery(self, request, queryset):
+        queryset.update(free_delivery=True)
+
+    def mark_as_no_free_delivery(self, request, queryset):
+        queryset.update(in_stock=False)
+
+    def mark_as_limited_edition(self, request, queryset):
+        queryset.update(limited_edition=True)
+
+    def mark_as_unlimited_edition(self, request, queryset):
+        queryset.update(limited_edition=False)
+
+    mark_as_in_stock.short_description = 'Пометить как имеющиеся в наличии'
+    mark_as_out_of_stock.short_description = 'Пометить как отсутствующие'
+    mark_as_free_delivery.short_description = 'Пометить как имеющие свободную доставку'
+    mark_as_no_free_delivery.short_description = 'Пометить как не имеющие свободную доставку'
+    mark_as_limited_edition.short_description = 'Пометить как ограниченный тираж'
+    mark_as_unlimited_edition.short_description = 'Пометить как неограниченный тираж'
+
+
+class DescrPointAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'content']
+    list_filter = ['product']
+    search_fields = ['content']
+
+
+class AddInfoPointAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'characteristic', 'value']
+    list_filter = ['product']
+    search_fields = ['characteristic', 'value']
+
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Fabricator, FabricatorAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(DescrPoint, DescrPointAdmin)
+admin.site.register(AddInfoPoint, AddInfoPointAdmin)
--- online_store/app_catalog/models.py
+++ online_store/app_catalog/models.py
@@ -19,6 +19,7 @@ class Category(models.Model):
 
 
 class Fabricator(models.Model):
+    """ Модель производителя. """
     title = models.CharField(max_length=255, verbose_name='название')
 
     class Meta:
@@ -31,17 +32,51 @@ class Fabricator(models.Model):
 
 class Product(models.Model):
     """ Модель товара. """
-    categories = models.ManyToManyField(Category, related_name='products', verbose_name='категории')
     fabricator = models.ForeignKey(Fabricator, blank=True, null=True, on_delete=models.SET_NULL,
                                    related_name='products', verbose_name='производитель')
+    categories = models.ManyToManyField(Category, related_name='products', verbose_name='категории')
     title = models.CharField(max_length=255, verbose_name='название')
     slug = models.SlugField(max_length=255, unique_for_date='added_at')
-    description = models.CharField(max_length=100, verbose_name='описание')
+    description = models.TextField(verbose_name='описание')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
-    num_purchases = models.PositiveIntegerField(default=0, verbose_name='количество покупок')
     image = models.ImageField(upload_to='images/products/', blank=True, null=True, verbose_name='иконка')
+    added_at = models.DateTimeField(verbose_name='дата публикации')
+    num_purchases = models.PositiveIntegerField(default=0, verbose_name='количество покупок')
     sort_index = models.PositiveIntegerField(verbose_name='индекс сортировки')
     in_stock = models.BooleanField(default=False, verbose_name='в наличии')
     free_delivery = models.BooleanField(default=False, verbose_name='с бесплатной доставкой')
     limited_edition = models.BooleanField(default=False, verbose_name='ограниченный тираж')
-    added_at = models.DateTimeField(verbose_name='дата публикации')
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+
+    def __str__(self):
+        return self.title
+
+
+class DescrPoint(models.Model):
+    """ Модель пункта описания товара. """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='descr_points', verbose_name='товар')
+    content = models.CharField(max_length=255, verbose_name='содержание')
+
+    class Meta:
+        verbose_name = 'пункт описания'
+        verbose_name_plural = 'пункты описания'
+
+    def __str__(self):
+        return self.content
+
+
+class AddInfoPoint(models.Model):
+    """ Модель пункта дополнительной информации о товаре. """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='add_info_points', verbose_name='товар')
+    characteristic = models.CharField(max_length=255, verbose_name='характеристика')
+    value = models.CharField(max_length=255, verbose_name='значение')
+
+    class Meta:
+        verbose_name = 'пункт доп. информации'
+        verbose_name_plural = 'пункты доп. информации'
+
+    def __str__(self):
+        return '{characteristic}: {value}'.format(characteristic=self.characteristic, value=self.value)
