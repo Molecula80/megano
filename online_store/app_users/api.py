2356180692986b43e3e79b93997d5e37438250bb
Asus <molecula80@gmail.com>
2022-10-29 17:06:59 +0300

    use PermissionRequiredMixin

--- online_store/app_users/api.py
+++ online_store/app_users/api.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
@@ -8,8 +8,9 @@ from .models import User
 from .serializers import UserSerializer
 
 
-class UserListApi(ListModelMixin, CreateModelMixin, GenericAPIView):
+class UserListApi(PermissionRequiredMixin, ListModelMixin, CreateModelMixin, GenericAPIView):
     """ Представление для получения списка пользователей и создания нового пользователя. """
+    permission_required = ('app_catalog.view_user', 'app_catalog.add_user')
     queryset = User.objects.prefetch_related('groups').all()
     serializer_class = UserSerializer
     filterset_fields = ['is_superuser', 'full_name', 'email', 'is_staff', 'is_active']
@@ -21,36 +22,27 @@ class UserListApi(ListModelMixin, CreateModelMixin, GenericAPIView):
         :return: список пользователей
         :rtype: list
         """
-        if not request.user.has_perm('app_users.view_user'):
-            raise PermissionDenied()
         return self.list(request)
 
     def post(self, request, format=None):
         """ Метод для создания нового пользователя. """
-        if not request.user.has_perm('app_users.add_user'):
-            raise PermissionDenied()
         return self.create(request)
 
 
-class UserDetailApi(UpdateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericAPIView):
+class UserDetailApi(PermissionRequiredMixin, UpdateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericAPIView):
     """ Представление для получения детальной информации о пользователе, а также его редактирования и удаления. """
+    permission_required = ('app_catalog.view_user', 'app_catalog.change_user', 'app_catalog.delete_user')
     queryset = User.objects.prefetch_related('groups').all()
     serializer_class = UserSerializer
 
     def get(self, request, *args, **kwargs):
         """ Метод для получения детальной информации о пользователе. """
-        if not request.user.has_perm('app_users.view_user'):
-            raise PermissionDenied()
         return self.retrieve(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
         """ Метод для редактирования пользователя. """
-        if not request.user.has_perm('app_users.change_user'):
-            raise PermissionDenied()
         return self.update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
         """ Метод для удаления пользователя. """
-        if not request.user.has_perm('app_users.delete_user'):
-            raise PermissionDenied()
         return self.destroy(request, *args, **kwargs)
