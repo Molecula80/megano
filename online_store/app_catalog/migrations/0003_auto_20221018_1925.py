69bc92961bc9ea61ca2bb618c3e3701f73eb263d
Asus <molecula80@gmail.com>
2022-10-30 20:01:59 +0300

    make new migrations

--- online_store/app_catalog/migrations/0003_auto_20221018_1925.py
+++ online_store/app_catalog/migrations/0003_auto_20221018_1925.py
@@ -50,29 +50,4 @@ class Migration(migrations.Migration):
                 'verbose_name_plural': 'товары',
             },
         ),
-        migrations.CreateModel(
-            name='DescrPoint',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('content', models.CharField(max_length=255, verbose_name='содержание')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='descr_points', to='app_catalog.Product', verbose_name='товар')),
-            ],
-            options={
-                'verbose_name': 'пункт описания',
-                'verbose_name_plural': 'пункты описания',
-            },
-        ),
-        migrations.CreateModel(
-            name='AddInfoPoint',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('characteristic', models.CharField(max_length=255, verbose_name='характеристика')),
-                ('value', models.CharField(max_length=255, verbose_name='значение')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='add_info_points', to='app_catalog.Product', verbose_name='товар')),
-            ],
-            options={
-                'verbose_name': 'пункт доп. информации',
-                'verbose_name_plural': 'пункты доп. информации',
-            },
-        ),
     ]
