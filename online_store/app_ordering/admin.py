1b3efa3e0b60bb9146670f0c2f6f708b15b681f8
Asus <molecula80@gmail.com>
2023-01-30 22:38:42 +0300

    add order models to admin site

--- online_store/app_ordering/admin.py
+++ online_store/app_ordering/admin.py
@@ -1,5 +1,55 @@
 from django.contrib import admin
-from .models import DeliveryMethod
+from .models import Order, OrderItem, DeliveryMethod
+
+
+class OrderItemInline(admin.TabularInline):
+    """ Инлайн класс для редактирования пунктов описания товаров. """
+    model = OrderItem
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """ Административная модель товара. """
+    list_display = ['id', 'user', 'full_name', 'telephone', 'email', 'delivery_method', 'city', 'payment_method',
+                    'paid', 'created']
+    list_filter = ['delivery_method', 'payment_method', 'paid']
+    search_fields = ['id', 'full_name', 'email']
+    inlines = [OrderItemInline]
+    actions = ['mark_as_paid', 'mark_as_unpaid', 'mark_as_pending_payment']
+    fieldsets = (
+        ('Параметры пользователя', {
+            'fields': ('user', 'full_name', 'telephone', 'email')
+        }),
+        ('Параметры доставки и оплаты', {
+            'fields': ('delivery_method', 'city', 'address', 'payment_method', 'comment'),
+            'classes': ['collapse']
+        }),
+        ('Статус заказа', {
+            'fields': ('paid', 'error_message'),
+            'classes': ['collapse']
+        })
+    )
+
+    def mark_as_paid(self, request, queryset) -> None:
+        """ Помечает заказы как оплаченные. """
+        queryset.update(paid=True)
+
+    def mark_as_unpaid(self, request, queryset) -> None:
+        """ Помечает заказы как не оплаченные. """
+        queryset.update(paid=False)
+
+    def mark_as_pending_payment(self, request, queryset):
+        """ Помечает заказы как ожилающие оплаты. """
+        queryset.update(paid=None)
+
+    mark_as_paid.short_description = 'Пометить как оплаченные'
+    mark_as_unpaid.short_description = 'Пометить как неоплаченные'
+    mark_as_pending_payment.short_description = 'Пометить как ожидающие оплаты'
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    """ Административная модель единицы заказа. """
+    list_display = ['id', 'order', 'product', 'price', 'quantity']
 
 
 class DeliveryMethodAdmin(admin.ModelAdmin):
@@ -7,4 +57,6 @@ class DeliveryMethodAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'price', 'free_delivery_cost']
 
 
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(DeliveryMethod, DeliveryMethodAdmin)
--- online_store/app_ordering/models.py
+++ online_store/app_ordering/models.py
@@ -1,22 +1,51 @@
 from django.db import models
+
 from app_users.models import User
+from app_catalog.models import Product
 
 
 class Order(models.Model):
     """ Модель заказа. """
+    PAYMENT_METHOD_CHOICES = [(1, 'Онлайн картой'), (2, 'Онлайн со случайного чужого счета')]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders', verbose_name='пользователь')
-    date = models.DateField(auto_now_add=True, verbose_name='дата заказа')
+    created = models.DateField(auto_now_add=True, verbose_name='дата заказа')
     full_name = models.CharField(max_length=255, verbose_name='ФИО')
-    telephone = models.CharField(max_length=16, blank=True, null=True, verbose_name='телефон')
-    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=16, verbose_name='телефон')
+    email = models.EmailField()
     delivery_method = models.ForeignKey('DeliveryMethod', null=True, on_delete=models.SET_NULL, related_name='orders',
                                         verbose_name='способ доставки')
     city = models.CharField(max_length=255, verbose_name='город')
     address = models.TextField(verbose_name='адрес')
-    PAYMENT_METHOD_CHOICES = [(1, 'Онлайн картой'), (2, 'Онлайн со случайного чужого счета')]
     payment_method = models.CharField(max_length=1, choices=PAYMENT_METHOD_CHOICES, default=1,
                                       verbose_name='способ оплаты')
-    comment = models.TextField(verbose_name='коментарий')
+    comment = models.TextField(blank=True, null=True, verbose_name='коментарий')
+    paid = models.NullBooleanField(default=None, verbose_name='оплачено')
+    error_message = models.TextField(blank=True, null=True, default=None, verbose_name='сообщение об ошибке')
+
+    class Meta:
+        verbose_name = 'заказ'
+        verbose_name_plural = 'заказы'
+
+    def __str__(self):
+        """ Возвращает ID заказа. """
+        return '{}'.format(self.id)
+
+
+class OrderItem(models.Model):
+    """ Модель единицы заказа. """
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items', verbose_name='заказ')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items', verbose_name='товар')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
+    quantity = models.PositiveIntegerField(verbose_name='количество')
+
+    class Meta:
+        verbose_name = 'единица заказа'
+        verbose_name_plural = 'единицы заказа'
+
+    def __str__(self):
+        """ Возвращает ID заказа и название продукта. """
+        return '{order}, {product}'.format(order=self.order.id, product=self.product.title)
 
 
 class DeliveryMethod(models.Model):
